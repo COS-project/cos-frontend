63670009c21114020b223db5889d8a65d380c9af
Brokyeom <goindol40@gmail.com>
2023-09-29 16:26:55 +0900

    chore : 불필요한 코드 삭제

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import './globals.css';
 
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -12,8 +9,8 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className="flex flex-column justify-center">
-      <body className={inter.className}>{children}</body>
+    <html className="flex flex-column justify-center">
+      <body>{children}</body>
     </html>
   );
 }
