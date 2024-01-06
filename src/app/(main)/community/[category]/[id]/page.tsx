da434551e9d7a4d59142e6e8ac11505603bccced
Brokyeom <goindol40@gmail.com>
2024-01-06 10:22:25 +0900

    feat : 동적 라우팅 디렉토리 생성

---
+++ src/app/(main)/community/[category]/[id]/page.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function CommunityDetailPage() {
+  return <div>커뮤니티 디테일 페이지</div>;
+}
---
+++ src/app/(main)/community/[category]/page.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function CommunityCategoryPage() {
+  return <div>카테고리별 페이지</div>;
+}
