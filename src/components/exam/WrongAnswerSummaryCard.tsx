7beef1ac19d1ef05ffa3a3cee3a5bd8f87e557b3
LSH <121619411+Choguesung@users.noreply.github.com>
2024-01-07 16:16:57 +0900

    style : WrongAnswerSummaryCard
    수평 마진 수정
    상수 -> auto

--- src/components/exam/WrongAnswerSummaryCard.tsx
+++ src/components/exam/WrongAnswerSummaryCard.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 const WrongQuestionsSummaryBox = () => {
   return (
-    <div className="mx-5 mt-4 w-[90%] p-3 rounded-3xl bg-blue">
+    <div className="mx-auto mt-4 w-[90%] p-3 rounded-3xl bg-blue">
       <div className="px-2">
         <div className="text-white font-bold text-left text-h4 my-1">지금까지 틀린문제만 모아봤어요.</div>
         <Link href="/exam/wrong">
