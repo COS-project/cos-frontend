077f1b0325542950458ca744da7b1268f9a2b7c5
LSH <121619411+Choguesung@users.noreply.github.com>
2024-01-08 22:03:51 +0900

    style: Timermodal
    시험 타이머 모달 스타일 초기 세팅

--- src/components/exam/TimerModal.tsx
+++ src/components/exam/TimerModal.tsx
@@ -1,9 +1,15 @@
-const SessionModal = () => {
+interface SessionModalProps {
+  closeModal: () => void;
+}
+
+const TimerModal: React.FC<SessionModalProps> = ({ closeModal }) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-30">
-      <div>asd</div>
+      <div className="w-[80%]">
+        <button className="w-full flex justify-end text-white text-h6 px-2 my-2">닫기 X</button>
+      </div>
     </div>
   );
 };
 
-export default SessionModal;
+export default TimerModal;
