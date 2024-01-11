d83b5abb27a793e132920b13e451a1e46358c21f
rusia9217 <110519493+unnimm@users.noreply.github.com>
2024-01-11 11:26:42 +0900

    feat: navbar, header 자격 응시 정보 페이지에서 보이지 않게
    recoil을 활용하여 자격 응시 정보 페이지에서 navbar, header 보이지 않도록, 홈화면에서는 보이도록 구현하였습니다.

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Link from 'next/link';
+import { useRecoilState } from 'recoil';
 
 import Header from '@/components/common/Header';
 import NavBar from '@/components/common/NavBar';
@@ -8,10 +9,8 @@ import NavBar from '@/components/common/NavBar';
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
     <div>
-      <Header />
       {children}
       <Link href="/">온보딩 화면으로 아동하기</Link>
-      <NavBar />
     </div>
   );
 }
