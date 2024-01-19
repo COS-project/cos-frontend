84e76028d87da1b4fa603db1e19dae9bb09f962a
ummiih <156565796+hwangyummii@users.noreply.github.com>
2024-01-19 23:31:04 +0900

    style: 디자인 변경에 따른 css 변경
    FilterModal width, padding 변경

--- src/components/common/FilterModal.tsx
+++ src/components/common/FilterModal.tsx
@@ -16,12 +16,12 @@ interface Props {
 const FilterModal = (props: Props) => {
   const { data, className, setDataState, setIsOpen } = props;
   return (
-    <div className={twMerge('border-[1px] border-gray2 bg-white rounded-[16px] w-fit py-2', className)}>
+    <div className={twMerge('border-[1px] border-gray2 bg-white rounded-[16px] py-2 z-10', className)}>
       {data.map((datum, index) => {
         return (
           <div
             key={index}
-            className="text-h4 text-gray3 py-[6px] px-3 hover:text-black transition"
+            className="text-h4 text-gray3 py-3 px-4 hover:text-black transition"
             onClick={() => {
               setDataState(datum);
               setIsOpen(false);
