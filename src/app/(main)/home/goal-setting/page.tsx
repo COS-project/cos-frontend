734cbcb26252c68c1ea7ff3c5294a46d7cccf801
ummiih <156565796+hwangyummii@users.noreply.github.com>
2024-01-22 04:32:53 +0900

    feat: 목표 설정 페이지 Axios POST request

--- src/app/(main)/home/goal-setting/page.tsx
+++ src/app/(main)/home/goal-setting/page.tsx
@@ -1,15 +1,22 @@
 'use client';
 
 import * as React from 'react';
+import { useRecoilValue } from 'recoil';
 
+import Button from '@/components/common/Button';
 import PreparationPeriodSetting from '@/components/home/goal-setting/PreparationPeriodSetting';
 import SelectCertification from '@/components/home/goal-setting/SelectCertification';
 import SetDailyGoals from '@/components/home/goal-setting/SetDailyGoals';
 import SetGoalScore from '@/components/home/goal-setting/SetGoalScore';
+import { postGoalSettingData } from '@/lib/api/home';
+import { goalSettingState } from '@/recoil/home/atom';
 
 const GoalSetting = () => {
+  const goalSettingData = useRecoilValue(goalSettingState);
+
   return (
     <div className="flex flex-col gap-y-8 mx-5">
+      <Button onClick={() => {postGoalSettingData(goalSettingData);}} className={'absolute right-0 w-fit'}>저장</Button>
       {/*자격증 선택*/}
       <SelectCertification />
       {/*목표 점수 설정*/}
@@ -22,4 +29,3 @@ const GoalSetting = () => {
   );
 };
 export default GoalSetting;
-
