39b1fd55d25682552e61416a0b793a7edd712c9a
ummiih <156565796+hwangyummii@users.noreply.github.com>
2024-01-22 04:28:21 +0900

    refactor: Button 컴포넌트 수정
    *refactor: onStep, Icon이 없어도 Button을 사용할 수 있도록 코드 수정
    
    *refactor: 변수명 수정 (onMove > onClick)

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -7,22 +7,20 @@ interface Props {
   children?: React.ReactNode;
   className: string;
   Icon?: (props: React.SVGProps<SVGSVGElement>) => JSX.Element;
-  onMove: () => void;
-  onStep: React.Dispatch<React.SetStateAction<string>>;
+  onClick: (data?: Object) => void;
+  onStep?: React.Dispatch<React.SetStateAction<string>>;
 }
 const Button = (props: Props) => {
-  const { children, className, Icon, onMove, onStep } = props;
+  const { children, className, Icon, onClick, onStep } = props;
   return (
     <button
       onClick={() => {
-        onMove();
-        onStep;
+        onClick();
+        onStep ? onStep : null;
       }}
       className={twMerge('bg-blue-button', className)}>
       <div>{children}</div>
-      <div className="mt-[2px]">
-        <Icon />
-      </div>
+      <div className="mt-[2px]">{Icon ? <Icon /> : null}</div>
     </button>
   );
 };
