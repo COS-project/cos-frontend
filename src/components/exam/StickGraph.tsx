3a7866a6d3ef96a8a097dc485f5e2d34e7807c2a
LSH <121619411+Choguesung@users.noreply.github.com>
2024-01-24 19:48:13 +0900

    Update CorrectRateGraph.tsx

--- src/components/exam/StickGraph.tsx
+++ src/components/exam/StickGraph.tsx
@@ -3,7 +3,7 @@ interface StickGraphProps {
   color: string;
 }
 
-// report 페이지에서 쓰이는 그래프 컴포넌트입니다
+// report 페이지에서 쓰이는 그래프 컴포넌트
 const StickGraph: React.FC<StickGraphProps> = ({ height, color }) => {
   return <div style={{ height: `${height}%` }} className={`w-[20%] mt-auto bg-${color} rounded-t-full`}></div>;
 };
