54afb6e0d6b676b9abae785a466a2f0b6015670c
LSH <121619411+Choguesung@users.noreply.github.com>
2024-03-02 21:16:35 +0900

    refactor : data구조

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -5,7 +5,7 @@ const client = axios.create({
   headers: {
     'Content-type': 'application/json',
     'Access-Token':
-      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJyb2xlcyI6WyJST0xFX0dVRVNUIl0sImVtYWlsIjoidGtkZ2g2NDI3QG5hdmVyLmNvbSIsInN1YiI6InRrZGdoNjQyN0BuYXZlci5jb20iLCJpYXQiOjE3MDkyOTI4MTQsImV4cCI6MTcwOTU1MjAxNH0.fJ6M1EQpE2Z79w8HLfNeH7aLKDRFSrjIxlyHDp1yl7U',
+      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJyb2xlcyI6WyJST0xFX0dVRVNUIl0sImVtYWlsIjoidGtkZ2g2NDI3QG5hdmVyLmNvbSIsInN1YiI6InRrZGdoNjQyN0BuYXZlci5jb20iLCJpYXQiOjE3MDkzNzc3NjUsImV4cCI6MTcwOTYzNjk2NX0.XgO7njjRlfGO1Iu00_ykR5u8OL_yWPSanU3RqUlCvBc',
   },
   withCredentials: true,
 });
