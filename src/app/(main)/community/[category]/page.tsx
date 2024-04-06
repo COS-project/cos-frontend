ada47064eddfe0445e8e9a718d2797ea98809c88
Brokyeom <goindol40@gmail.com>
2024-04-06 16:20:12 +0900

    feat : 후기 게시판 배너 포함 퍼블리싱

--- src/app/(main)/community/[category]/page.tsx
+++ src/app/(main)/community/[category]/page.tsx
@@ -1,17 +1,13 @@
 'use client';
 
-import { usePathname, useRouter } from 'next/navigation';
-
+import Banner from '@/components/common/Banner';
 import ReviewCard from '@/components/community/review/ReviewCard';
 
 export default function CommunityCategoryPage() {
-  const pathname = usePathname();
-  const router = useRouter();
-
   return (
-    <main className="bg-gray0 px-[2rem] pb-[8rem]">
+    <main className="bg-gray0 px-[2rem] pt-[2rem] pb-[8rem]">
+      <Banner title="크루들에게 따끈후기를 공유해주세요!" buttonText="따끈 후기 작성" href={''} />
       <ReviewCard />
-      <button onClick={() => router.replace(`${pathname}/1`)}>게시글 상세로 이동하기</button>
     </main>
   );
 }
