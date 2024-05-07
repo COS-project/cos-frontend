790324899e20c9dc4084dda272b1a13af0d67f0b
ummiih <156565796+hwangyummii@users.noreply.github.com>
2024-05-08 02:40:22 +0900

    feat: buttom/top element, 생성, page #103

--- src/app/(main)/mypage/writing/page.tsx
+++ src/app/(main)/mypage/writing/page.tsx
@@ -1,66 +1,74 @@
-import NavBar from '@/components/common/NavBar';
+'use client';
+
+import { useState } from 'react';
+
+import MyWritingMenu from '@/components/mypage/MyWritingMenu';
+import Post from '@/components/mypage/Post';
 
 export default function MyWriting() {
+  // REVIEW, COMMENTARY, TIP, NORMAL
+  const [boardType, setBoardType] = useState<string>('REVIEW');
+
+  const commentaryElement = () => {
+    return (
+      <div className={'flex gap-x-[6px] pb-3'}>
+        <div className={'px-2 py-[2px] text-gray4 bg-gray0 rounded-[8px]'}>2023년도</div>
+        <div className={'px-2 py-[2px] text-gray4 bg-gray0 rounded-[8px]'}>1회차</div>
+        <div className={'px-2 py-[2px] text-gray4 bg-gray0 rounded-[8px]'}>23번</div>
+      </div>
+    );
+  };
+
+  const tipElement = () => {
+    return (
+      <div className={'pb-2'}>
+        <div className={'px-3 py-[2px] text-white bg-primary rounded-full w-fit font-light'}>BEST</div>
+      </div>
+    );
+  };
+
+  const bottomElement = () => {
+    return (
+      <div className={'flex justify-end gap-x-2'}>
+        <button className={'bg-gray0 py-2 px-4 rounded-[12px]'}>수정</button>
+        <button className={'bg-black text-white py-2 px-4 rounded-[12px]'}>삭제</button>
+      </div>
+    );
+  };
+
   return (
     <>
-      <div className="w-full mt-2">
-        <header className="flex justify-between items-center px-[1.25rem] py-[0.25rem] text-h4">
-          <a href="../mypage">{'<'}</a>
-          <div>내가 쓴 글</div>
-          <div>...</div>
-        </header>
-        <div className="flex justify-around mt-2">
-          <button className="rounded-2xl border border-gray2 px-3 py-1 text-h6 focus:bg-black focus:text-white mx-1">
-            따끈후기
-          </button>
-          <button className="rounded-2xl border border-gray2 px-3 py-1 text-h6 focus:bg-black focus:text-white mx-1">
-            해설게시판
-          </button>
-          <button className="rounded-2xl border border-gray2 px-3 py-1 text-h6 focus:bg-black focus:text-white mx-1">
-            꿀팁게시판
-          </button>
-          <button className="rounded-2xl border border-gray2 px-3 py-1 text-h6 focus:bg-black focus:text-white mx-1">
-            자유게시판
-          </button>
+      <div className={'flex flex-col gap-y-4 bg-gray0 min-h-screen'}>
+        <MyWritingMenu boardType={boardType} setBoardType={setBoardType} />
+        <div className={'px-5 flex flex-col gap-y-4 '}>
+          <Post
+            content={'어려운 문제였어요. 데이터베이스 관계형 데이터 베이스...'}
+            title={'스프레드시트 23번 문제'}
+            commentCount={32}
+            bottomElement={bottomElement}
+            createdAt={'2023.7.12'}
+            imageUrl={'/황유림.jpg'}
+            likeCount={12}
+            topElement={commentaryElement}></Post>
+          <Post
+            content={'어려운 문제였어요. 데이터베이스 관계형 데이터 베이스...'}
+            title={'스프레드시트 23번 문제'}
+            commentCount={32}
+            bottomElement={bottomElement}
+            createdAt={'2023.7.12'}
+            imageUrl={'/황유림.jpg'}
+            likeCount={12}></Post>
+          <Post
+            content={'어려운 문제였어요. 데이터베이스 관계형 데이터 베이스...'}
+            title={'스프레드시트 23번 문제'}
+            commentCount={32}
+            bottomElement={bottomElement}
+            createdAt={'2023.7.12'}
+            imageUrl={'/황유림.jpg'}
+            likeCount={12}
+            topElement={tipElement}></Post>
         </div>
       </div>
-
-      <div className="w-full bg-gray1  mx-2 mt-3 px-3 py-3">
-        <select className="rounded-xl text-h6 text-gray4">
-          <option key="all" value="all">
-            전체
-          </option>
-          <option key="new" value="new">
-            최신순
-          </option>
-          <option key="old" value="old">
-            작성순
-          </option>
-        </select>
-        {/* 여기가 박스안에 내용들 담아놓은곳 아래.. */}
-        <div className="w-full bg-white rounded-lg mt-3 py-2">
-          <div className="flex justify-around items-center px-3 py-[0.25rem]">
-            <div className="text-h6">nickname</div>
-            <div className="flex justify-between">
-              <div className="rounded-2xl text-h6 border border-gray4 py-[2px] px-1 text-gray4 mx-2">
-                준비기간 6개월 ↑
-              </div>
-              <div className="rounded-2xl text-h6 text-white bg-point py-[2px] px-1">조금 어려워요</div>
-            </div>
-          </div>
-          <div>
-            <div className="px-6 mt-2">
-              <div className="text-h6 text-black">막 생각보다 어렵지는 않음</div>
-              <div className="text-gray4 text-h7">작성일 2024.03.16</div>
-            </div>
-            <div className="flex justify-end mx-3 mt-2">
-              <button className="rounded-xl bg-[#F0F0F0] px-3 py-2">수정</button>
-              <button className="rounded-xl bg-black text-white px-3 py-2 ml-2">삭제</button>
-            </div>
-          </div>
-        </div>
-      </div>
-      <NavBar />
     </>
   );
 }
