629525aac9fdde56c8d17e59a72d3d506febf983
ummiih <156565796+hwangyummii@users.noreply.github.com>
2024-05-09 22:44:29 +0900

    style: header, navbar 설정 #105

--- src/app/(main)/alarm/page.tsx
+++ src/app/(main)/alarm/page.tsx
@@ -1,42 +1,37 @@
+import React, { SVGProps } from 'react';
+
 import Header from '@/components/common/Header';
 import NavBar from '@/components/common/NavBar';
 
 export default function Alarm() {
   return (
     <>
-      <div className="w-full mt-2">
-        <header className="w-full flex justify-between items-center px-[1.25rem] py-[0.25rem] text-h4">
-          <button>{'<'}</button>
-          <div>내가 쓴 글</div>
-          <button>검색</button>
-        </header>
-
-        <div className="w-full bg-gray0  mx-2 mt-3 px-3 py-3">
-          {/* 여기가 박스안에 내용들 담아놓은곳 아래.. */}
-          <div className="w-full bg-white rounded-xl mt-3 px-3 py-2">
-            <div>
-              <div className="text-h6 text-gray2">10분전</div>
-              <div className="px-1 mt-1">
-                <div className="text-h6 text-black w-[300px]">
-                  관심자격증 <b>정보처리기사</b> 후기를 작성해주세요!
-                </div>
-                <button className="border border-gray2 rounded-2xl text-h7 px-[6px]">따끈후기 작성하기{'>'}</button>
-              </div>
-            </div>
-          </div>
-
-          <div className="w-full bg-white rounded-xl mt-3 px-3 py-2">
-            <div className="text-h6 text-gray2">1시간전</div>
-            <div className="px-1 mt-1">
-              <div className="text-h6 text-black w-[300px]">
-                오늘의 목표 <b>'모의고사 2회 풀기'</b> 를 달성해볼까요?
-              </div>
-              <button className="border border-gray2 rounded-2xl text-h7 px-[6px]">모의고사보기{'>'}</button>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Header headerType={'dynamic'} title={'알림'} rightElement={<EmptyIcon />} />
+      <div className={'bg-gray0 min-h-screen'}></div>
       <NavBar />
     </>
   );
 }
+
+const EmptyIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width={32} height={32} fill="none" {...props}>
+    <mask
+      id="a"
+      width={32}
+      height={32}
+      x={0}
+      y={0}
+      maskUnits="userSpaceOnUse"
+      style={{
+        maskType: 'alpha',
+      }}>
+      <path fill="#D9D9D9" d="M0 0h32v32H0z" />
+    </mask>
+    <g mask="url(#a)">
+      <path
+        fill="#fff"
+        d="m26.11 26.867-8.474-8.474a7 7 0 0 1-2.306 1.349 7.8 7.8 0 0 1-2.64.468q-3.19 0-5.402-2.206-2.211-2.207-2.211-5.384T7.283 7.23q2.207-2.21 5.383-2.21t5.397 2.207q2.22 2.208 2.22 5.385 0 1.378-.492 2.692a7.2 7.2 0 0 1-1.347 2.28l8.48 8.45zm-13.424-7.785q2.721 0 4.595-1.87 1.875-1.869 1.875-4.597 0-2.727-1.875-4.597t-4.595-1.87q-2.726 0-4.604 1.87-1.877 1.871-1.877 4.597 0 2.728 1.877 4.597t4.603 1.87"
+      />
+    </g>
+  </svg>
+);
