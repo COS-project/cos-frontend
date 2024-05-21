e3c100027b650ef65421242550db7ab7e277193b
ummiih <156565796+hwangyummii@users.noreply.github.com>
2024-05-21 12:20:05 +0900

    style: 스타일 값이 맞지 않는 부분 수정 #113

--- src/app/(main)/exam/page.tsx
+++ src/app/(main)/exam/page.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 
 import Banner from '@/components/common/Banner';
 import Header from '@/components/common/Header';
-import NavBar from '@/components/common/NavBar';
-import WrongQuestionsSummaryBox from '@/components/exam/IncorrectQuestionSummaryCard';
 import SubjectSessionCard from '@/components/exam/SubjectList';
 import YearSelector from '@/components/exam/YearSelector';
 
@@ -19,12 +17,12 @@ const Exam = () => {
 const SolveExamBox = () => {
   return (
     <div>
-      <div className="w-[90%] mx-auto">
-        <div className="w-[95%] flex mx-auto space-x-1">
+      <div className="px-5 py-4">
+        <div className="flex gap-x-4 justify-between">
           <Banner title="지금까지 틀린 문제만 모아봤어요." buttonText="틀린 문제 풀기" href="/exam/wrong" />
-          <Banner title="실제 출제된 문제를 모아봤어요" buttonText="랜덤 모의고사" href="/exam/wrong" />
+          <Banner title="실제 출제된 문제를 모아봤어요." buttonText="랜덤 모의고사" href="/exam/wrong" />
         </div>
-        <div className="w-[90%] mx-auto font-black text-h4 mt-5">모의고사 풀기</div>
+        <div className="text-h3 mt-[24px] font-bold">모의고사 풀기</div>
         <YearSelector />
         <SubjectSessionCard />
       </div>
