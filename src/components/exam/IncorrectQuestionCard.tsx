74df70c5b6debb7fe5cc47f9bfc4d7c2ddf4f838
ummiih <156565796+hwangyummii@users.noreply.github.com>
2024-07-18 19:43:53 +0900

    feat: 틀린문제 모아보기에서 문제 삭제 기능 구현  #148

--- src/components/exam/IncorrectQuestionCard.tsx
+++ src/components/exam/IncorrectQuestionCard.tsx
@@ -1,10 +1,12 @@
 'use client';
-import React, { useState } from 'react';
+import React, { SVGProps, useState } from 'react';
 import { useRecoilState } from 'recoil';
+import { KeyedMutator } from 'swr';
 
 import QuestionContent from '@/components/exam/QuestionContent';
+import { deleteIncorrectQuestion } from '@/lib/api/exam';
 import { userAnswerRequests } from '@/recoil/exam/atom';
-import { Question, QuestionOptions, ReviewIncorrectMockExam, UserAnswerRequests } from '@/types/global';
+import { QuestionOptions, ReviewIncorrectAnswers, ReviewIncorrectMockExam, UserAnswerRequests } from '@/types/global';
 
 interface Props {
   selectOptionSeq: number;
@@ -13,11 +15,22 @@ interface Props {
   questionOptions: QuestionOptions[];
   questionText: string;
   questionSeq: number;
+  userAnswerId?: number; //틀린문제 모아보기만
+  incorrectQuestionsMutate?: KeyedMutator<ReviewIncorrectAnswers[]>; //틀린문제 모아보기만
 }
 
 const IncorrectQuestionCard = (props: Props) => {
   const [isOpen, setIsOpen] = useState(false);
-  const { mockExam, questionSeq, questionText, questionOptions, correctOption, selectOptionSeq } = props;
+  const {
+    mockExam,
+    questionSeq,
+    questionText,
+    questionOptions,
+    correctOption,
+    selectOptionSeq,
+    userAnswerId,
+    incorrectQuestionsMutate,
+  } = props;
   const [userAnswer, setUserAnswer] = useRecoilState<UserAnswerRequests>(userAnswerRequests);
   const [correctAnswer, setCorrectAnswer] = useState(0);
   const [selectedAnswerInThePast, setSelectedAnswerInThePast] = useState(0);
@@ -60,8 +73,22 @@ const IncorrectQuestionCard = (props: Props) => {
 
   return (
     <div className={'flex flex-col bg-white rounded-[32px] p-5 gap-y-[8px]'}>
-      <div className={'flex justify-start bg-gray0 text-gray4 text-h6 px-2 w-fit rounded-[8px] py-1'}>
-        {mockExam.examYear}년도 {mockExam.round}회차 {questionSeq}번
+      <div className={'flex justify-between'}>
+        <div className={'flex justify-start bg-gray0 text-gray4 text-h6 px-2 w-fit rounded-[8px] py-1'}>
+          {mockExam.examYear}년도 {mockExam.round}회차 {questionSeq}번
+        </div>
+
+        {/* 삭제버튼 */}
+        {userAnswerId && incorrectQuestionsMutate ? (
+          <button
+            onClick={() => {
+              deleteIncorrectQuestion(userAnswerId).then(() => {
+                incorrectQuestionsMutate();
+              });
+            }}>
+            <DeleteIcon />
+          </button>
+        ) : null}
       </div>
       <div className={'text-h3 font-semibold'}>{questionText}</div>
       <div className={'flex justify-center items-center'}>
@@ -131,3 +158,25 @@ function DropUpIcon(props: React.SVGProps<SVGSVGElement>) {
     </svg>
   );
 }
+const DeleteIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width={25} height={24} fill="none" {...props}>
+    <mask
+      id="a"
+      width={25}
+      height={24}
+      x={0}
+      y={0}
+      maskUnits="userSpaceOnUse"
+      style={{
+        maskType: 'alpha',
+      }}>
+      <path fill="#D9D9D9" d="M.646 0h24v24h-24z" />
+    </mask>
+    <g mask="url(#a)">
+      <path
+        fill="#6E6F71"
+        d="M12.646 12.708 7.4 17.954a.5.5 0 0 1-.345.15.47.47 0 0 1-.363-.15.5.5 0 0 1-.16-.354.5.5 0 0 1 .16-.354L11.938 12 6.692 6.754a.5.5 0 0 1-.15-.344.47.47 0 0 1 .15-.364.5.5 0 0 1 .354-.16.5.5 0 0 1 .354.16l5.246 5.246 5.246-5.246a.5.5 0 0 1 .344-.15.47.47 0 0 1 .364.15.5.5 0 0 1 .16.354.5.5 0 0 1-.16.354L13.353 12l5.247 5.246a.5.5 0 0 1 .15.344.47.47 0 0 1-.15.364.5.5 0 0 1-.354.16.5.5 0 0 1-.354-.16z"
+      />
+    </g>
+  </svg>
+);
