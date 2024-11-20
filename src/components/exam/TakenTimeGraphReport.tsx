68de9a0e7c075b759bfc35f912f6550abd8703e3
ummiih <156565796+hwangyummii@users.noreply.github.com>
2024-11-20 13:14:50 +0900

    style: 막대그래프, 필터 수정 #158
    필터: max값 부여하여 그 전에 h값을 fit하게 만듦
    막대그래프: 전체 제한 시간 중 머문 시간의 비율을 백분율로 측정하여 그래프에 표시

--- src/components/exam/TakenTimeGraphReport.tsx
+++ src/components/exam/TakenTimeGraphReport.tsx
@@ -6,7 +6,7 @@ interface Props {
   subjectResults: SubjectResultsType[];
   averageSubjectList: AverageSubjectInfoType[];
   timeLimit: number;
-  totalTakenTime: () => number;
+  totalTakenTime: number;
 }
 const TakenTimeGraphReport = (props: Props) => {
   const { subjectResults, averageSubjectList, timeLimit, totalTakenTime } = props;
@@ -27,7 +27,7 @@ const TakenTimeGraphReport = (props: Props) => {
         {/*걸린 시간*/}
         <div className={'pl-2'}>
           <div className={'text-h6'}>걸린시간</div>
-          <div className={'text-h3 font-semibold'}>{millisecondsToMinutes(totalTakenTime())}m</div>
+          <div className={'text-h3 font-semibold'}>{millisecondsToMinutes(totalTakenTime)}m</div>
         </div>
         {/*막대 그래프*/}
         <div className="flex items-center space-x-2">
@@ -40,9 +40,9 @@ const TakenTimeGraphReport = (props: Props) => {
               {subjectResults?.map((subjectResult, index) => {
                 return (
                   <div key={index} className="w-full flex justify-center space-x-2">
-                    <StickGraph height={millisecondsToMinutes(subjectResult.totalTakenTime)} color="second" />
+                    <StickGraph height={(subjectResult.totalTakenTime / timeLimit) * 100} color="second" />
                     <StickGraph
-                      height={millisecondsToMinutes(averageSubjectList ? averageSubjectList[index].totalTakenTime : 0)}
+                      height={(averageSubjectList[index]?.totalTakenTime || 0 / timeLimit) * 100}
                       color="gray2"
                     />
                   </div>
