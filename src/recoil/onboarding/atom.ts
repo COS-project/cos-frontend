b4cfd4fb13aabde36776e52f70b9c975bf1bd0ba
유림 <yulimh001@gmail.com>
2024-11-23 12:46:30 +0900

    fix: 타입 에러 수정

--- src/recoil/onboarding/atom.ts
+++ src/recoil/onboarding/atom.ts
@@ -5,7 +5,7 @@ import { atom } from 'recoil';
 import { PostInterestCertificate } from '@/types/global';
 
 //목표 설정 state
-export let interestCertificatesState = atom<string>({
+export let interestCertificatesState = atom<PostInterestCertificate>({
   key: 'interestCertificatesState',
   default: {
     interestTargetList: [],
