e1150e442486ba8029431e9bf89ac91958e61850
유림 <yulimh001@gmail.com>
2024-11-23 16:29:03 +0900

    style: 디자인에 맞게 수정 #168

--- src/components/stopwatch/AccumulatedTime.tsx
+++ src/components/stopwatch/AccumulatedTime.tsx
@@ -1,5 +1,7 @@
+import type { SVGProps } from 'react';
 import React from 'react';
 import { useRecoilState } from 'recoil';
+
 import { hStopwatchTimeState, mStopwatchTimeState, sStopwatchTimeState } from '@/recoil/stopwatch/atom';
 
 interface Props {
@@ -17,9 +19,8 @@ function AccumulatedTime(props: Props) {
   return (
     <>
       <div className="">
-        <div className="absolute z-40 w-full h-full px-5 py-8 bg-black bg-opacity-60 flex-col justify-center items-center gap-2 inline-flex ">
-          <div className="relative self-stretch px-2 justify-center items-center inline-flex">
-            <div className="w-52"></div>
+        <div className="absolute z-40 w-full h-full px-[39px] py-8 bg-black bg-opacity-60 flex-col justify-center items-center gap-2 inline-flex ">
+          <div className="relative self-stretch px-2 justify-end items-center inline-flex">
             <div
               className="flex cursor-pointer"
               onClick={() => {
@@ -28,41 +29,53 @@ function AccumulatedTime(props: Props) {
               <div className="text-center text-white text-base font-medium font-['Pretendard Variable'] leading-normal">
                 닫기
               </div>
-              <svg xmlns="http://www.w3.org/2000/svg" width="25" height="24" fill="none" viewBox="0 0 25 24">
-                <mask id="a" width="25" height="24" x="0" y="0" maskUnits="userSpaceOnUse">
-                  <path fill="#D9D9D9" d="M.646 0h24v24h-24z" />
-                </mask>
-                <g mask="url(#a)">
-                  <path
-                    fill="#fff"
-                    d="M12.646 12.708 7.4 17.954a.503.503 0 0 1-.345.15.467.467 0 0 1-.363-.15.49.49 0 0 1-.16-.354.49.49 0 0 1 .16-.354L11.938 12 6.692 6.754a.504.504 0 0 1-.15-.344.467.467 0 0 1 .15-.364.49.49 0 0 1 .354-.16.49.49 0 0 1 .354.16l5.246 5.246 5.246-5.246a.503.503 0 0 1 .344-.15.467.467 0 0 1 .364.15.49.49 0 0 1 .16.354.49.49 0 0 1-.16.354L13.353 12l5.247 5.246a.504.504 0 0 1 .15.344.467.467 0 0 1-.15.364.491.491 0 0 1-.354.16.49.49 0 0 1-.354-.16l-5.246-5.246Z"
-                  />
-                </g>
-              </svg>
+              <DeleteIcon />
             </div>
           </div>
-          <div className="px-5 py-6 w-[257px] bg-white rounded-[32px] flex-col justify-center gap-2 flex">
+          <section className="px-5 py-6 bg-white rounded-[32px] flex-col justify-center gap-2 flex w-full">
             <div className="self-stretch flex-col gap-4 flex">
               <div className="self-stretch flex-col gap-1 flex">
-                <div className="self-stretch text-neutral-900 text-h4 font-bold font-['Pretendard Variable'] leading-[30px]">
-                  전체 누적 시간
-                </div>
+                <div className="self-stretch text-h4 leading-[30px] font-medium">전체 누적 시간</div>
                 <div className="self-stretch text-primary text-h1 font-bold font-['Pretendard Variable'] leading-[30px]">
                   {hStopwatchTime}시간 {mStopwatchTime}분 {sStopwatchTime}초
                 </div>
-                <div className="self-stretch text-neutral-900 text-h4 font-bold font-['Pretendard Variable'] leading-[30px]">
+                <div className="self-stretch  text-h4 font-medium leading-[30px]">
                   <br />
                   목표 기간 동안의 누적 시간
                 </div>
                 <div className="self-stretch text-primary text-h1 font-bold font-['Pretendard Variable'] leading-[30px]">
+                  {/*TODO: 백엔드 수정되면 변경*/}
                   {hStopwatchTime}시간 {mStopwatchTime}분 {sStopwatchTime}초{' '}
                 </div>
               </div>
             </div>
-          </div>
+          </section>
         </div>
       </div>
     </>
   );
 }
 export default AccumulatedTime;
+
+const DeleteIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width={25} height={24} fill="none" {...props}>
+    <mask
+      id="a"
+      width={25}
+      height={24}
+      x={0}
+      y={0}
+      maskUnits="userSpaceOnUse"
+      style={{
+        maskType: 'alpha',
+      }}>
+      <path fill="#D9D9D9" d="M.974 0h24v24h-24z" />
+    </mask>
+    <g mask="url(#a)">
+      <path
+        fill="#fff"
+        d="m12.974 12.708-5.246 5.246a.5.5 0 0 1-.344.15.47.47 0 0 1-.364-.15.5.5 0 0 1-.16-.354.5.5 0 0 1 .16-.354L12.266 12 7.02 6.754a.5.5 0 0 1-.15-.344.47.47 0 0 1 .15-.364.5.5 0 0 1 .354-.16.5.5 0 0 1 .354.16l5.246 5.246 5.246-5.246a.5.5 0 0 1 .344-.15.47.47 0 0 1 .364.15.5.5 0 0 1 .16.354.5.5 0 0 1-.16.354L13.682 12l5.246 5.246a.5.5 0 0 1 .15.344.47.47 0 0 1-.15.364.5.5 0 0 1-.354.16.5.5 0 0 1-.354-.16z"
+      />
+    </g>
+  </svg>
+);
--- src/components/stopwatch/StopwatchAlert.tsx
+++ src/components/stopwatch/StopwatchAlert.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { type SVGProps } from 'react';
 import { useRecoilState } from 'recoil';
 
 import {
@@ -28,42 +28,33 @@ function StopwatchAlert(props: Props) {
   return (
     <>
       <div className="">
-        <div className="absolute z-40 w-full h-full px-5 py-8 bg-black bg-opacity-60 flex-col justify-center items-center gap-2 inline-flex ">
-          <div className="relative self-stretch px-2 justify-center items-center inline-flex">
-            <div className="w-64"></div>
+        <div className="absolute z-40 w-full h-full px-10 py-8 bg-black bg-opacity-60 flex-col justify-center items-center gap-2 inline-flex ">
+          {/* 닫기 버튼 */}
+          <div className="relative self-stretch px-2 items-center inline-flex justify-end mr-2">
             <div
-              className="flex cursor-pointer"
+              className=" flex cursor-pointer"
               onClick={() => {
                 setOnModal(false);
               }}>
               <div className="text-center text-white text-base font-medium font-['Pretendard Variable'] leading-normal">
                 닫기
               </div>
-              <svg xmlns="http://www.w3.org/2000/svg" width="25" height="24" fill="none" viewBox="0 0 25 24">
-                <mask id="a" width="25" height="24" x="0" y="0" maskUnits="userSpaceOnUse">
-                  <path fill="#D9D9D9" d="M.646 0h24v24h-24z" />
-                </mask>
-                <g mask="url(#a)">
-                  <path
-                    fill="#fff"
-                    d="M12.646 12.708 7.4 17.954a.503.503 0 0 1-.345.15.467.467 0 0 1-.363-.15.49.49 0 0 1-.16-.354.49.49 0 0 1 .16-.354L11.938 12 6.692 6.754a.504.504 0 0 1-.15-.344.467.467 0 0 1 .15-.364.49.49 0 0 1 .354-.16.49.49 0 0 1 .354.16l5.246 5.246 5.246-5.246a.503.503 0 0 1 .344-.15.467.467 0 0 1 .364.15.49.49 0 0 1 .16.354.49.49 0 0 1-.16.354L13.353 12l5.247 5.246a.504.504 0 0 1 .15.344.467.467 0 0 1-.15.364.491.491 0 0 1-.354.16.49.49 0 0 1-.354-.16l-5.246-5.246Z"
-                  />
-                </g>
-              </svg>
+              <DeleteIcon />
             </div>
           </div>
-          <div className="px-5 py-6 bg-white rounded-[32px] flex-col justify-center gap-2 flex">
+
+          {/* 모달창 */}
+          <section className="px-5 py-6 bg-white rounded-[32px] flex flex-col gap-2 w-full">
             <div className="self-stretch flex-col gap-4 flex">
-              <div className="self-stretch text-center flex-col gap-1 flex">
-                <div className="self-stretch text-neutral-900 text-h6 font-bold font-['Pretendard Variable'] leading-[30px]">
-                  누적 시간
-                </div>
-                <div className="self-stretch text-primary text-h2 font-bold font-['Pretendard Variable'] leading-[30px]">
+              <div className={'flex text-h2 font-bold gap-x-1'}>
+                <div className="self-stretch leading-[30px]">누적시간</div>
+                <div className="self-stretch text-primary leading-[30px]">
                   {hStopwatchTime}시간 {mStopwatchTime}분 {sStopwatchTime}초
                 </div>
-                <div className="self-stretch text-neutral-950 text-h6 font-normal font-['Pretendard Variable'] leading-[21px]">
-                  기록하시겠습니까?
-                </div>
+              </div>
+              <div>
+                <div className="text-h6 leading-[21px]">기록할까요?</div>
+                <div className="text-h6 leading-[21px]">기록하지 않으면 타이머의 시간이 초기화됩니다.</div>
               </div>
               <div className="justify-end items-center gap-2 inline-flex">
                 <div
@@ -76,12 +67,12 @@ function StopwatchAlert(props: Props) {
                     setIsReset(false); //테스트
                   }}>
                   <div className="text-gray4 text-h6 font-medium font-['Pretendard Variable'] leading-[21px]">
-                    아니요, 괜찮아요
+                    아니요
                   </div>
                 </div>
                 <div className="h-9 px-4 py-1 cursor-pointer bg-black rounded-[999px] justify-center items-center gap-2 flex">
                   <div
-                    className="text-white text-h6 font-medium font-['Pretendard Variable'] leading-[21px]"
+                    className="text-white text-h6 font-['Pretendard Variable'] leading-[21px]"
                     onClick={() => {
                       setOnAccumulatedModal(true); //모달창 열기
                       setOnModal(false); //현재창 닫기
@@ -90,15 +81,39 @@ function StopwatchAlert(props: Props) {
                       setIsReset(true); //테스트
                       setIsReset(false); //테스트
                     }}>
-                    네, 기록할게요
+                    기록하기
                   </div>
                 </div>
               </div>
             </div>
-          </div>
+          </section>
         </div>
       </div>
     </>
   );
 }
+
 export default StopwatchAlert;
+
+const DeleteIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width={25} height={24} fill="none" {...props}>
+    <mask
+      id="a"
+      width={25}
+      height={24}
+      x={0}
+      y={0}
+      maskUnits="userSpaceOnUse"
+      style={{
+        maskType: 'alpha',
+      }}>
+      <path fill="#D9D9D9" d="M.974 0h24v24h-24z" />
+    </mask>
+    <g mask="url(#a)">
+      <path
+        fill="#fff"
+        d="m12.974 12.708-5.246 5.246a.5.5 0 0 1-.344.15.47.47 0 0 1-.364-.15.5.5 0 0 1-.16-.354.5.5 0 0 1 .16-.354L12.266 12 7.02 6.754a.5.5 0 0 1-.15-.344.47.47 0 0 1 .15-.364.5.5 0 0 1 .354-.16.5.5 0 0 1 .354.16l5.246 5.246 5.246-5.246a.5.5 0 0 1 .344-.15.47.47 0 0 1 .364.15.5.5 0 0 1 .16.354.5.5 0 0 1-.16.354L13.682 12l5.246 5.246a.5.5 0 0 1 .15.344.47.47 0 0 1-.15.364.5.5 0 0 1-.354.16.5.5 0 0 1-.354-.16z"
+      />
+    </g>
+  </svg>
+);
