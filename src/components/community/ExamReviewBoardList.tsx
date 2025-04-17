3b5b121e9b47941f27b9aba44fe87e0b2918cbae
유림 <yulimh001@gmail.com>
2025-04-17 19:00:10 +0900

    fix: 에러발생시, 필터도 보이지 않도록 구현

--- src/components/community/ExamReviewBoardList.tsx
+++ src/components/community/ExamReviewBoardList.tsx
@@ -110,47 +110,49 @@ const ExamReviewBoardList = (props: Props) => {
           <UserActionReminder content={'크루들에게 따끈후기를 공유해주세요!'} button={userActionReminderMove()} />
         )}
         {/*filter*/}
-        <div className={'flex gap-x-2'}>
-          <div
-            onClick={() => setIsPreparePeriodOpen(!isPreparePeriodOpen)}
-            className={'relative flex items-center rounded-full bg-white py-[6px] px-[12px]'}>
-            <div className={'text-h6'}>
-              {selectedPreparePeriodContent === '전체' ? '준비기간 전체' : selectedPreparePeriodContent}
+        {examReviewsCRT_003Error || examReviewsCRT_004Error ? null : (
+          <div className={'flex gap-x-2'}>
+            <div
+              onClick={() => setIsPreparePeriodOpen(!isPreparePeriodOpen)}
+              className={'relative flex items-center rounded-full bg-white py-[6px] px-[12px]'}>
+              <div className={'text-h6'}>
+                {selectedPreparePeriodContent === '전체' ? '준비기간 전체' : selectedPreparePeriodContent}
+              </div>
+              {isPreparePeriodOpen ? <ActivationIcon /> : <DisableIcon />}
+              {isPreparePeriodOpen ? (
+                <PreparePeriodFilter
+                  setEndPreMonths={setEndPreMonths}
+                  setStartMonths={setStartMonths}
+                  setSelectedPreparePeriodContent={setSelectedPreparePeriodContent}
+                  setIsPreparePeriodOpen={setIsPreparePeriodOpen}
+                />
+              ) : null}
             </div>
-            {isPreparePeriodOpen ? <ActivationIcon /> : <DisableIcon />}
-            {isPreparePeriodOpen ? (
-              <PreparePeriodFilter
-                setEndPreMonths={setEndPreMonths}
-                setStartMonths={setStartMonths}
-                setSelectedPreparePeriodContent={setSelectedPreparePeriodContent}
-                setIsPreparePeriodOpen={setIsPreparePeriodOpen}
-              />
-            ) : null}
-          </div>
-          <div
-            onClick={() => setIsExamDifficultyOpen(!isExamDifficultyOpen)}
-            className={'relative flex items-center rounded-full bg-white py-[6px] px-[12px]'}>
-            <div className={'text-h6'}>
-              {selectedExamDifficultyContent === '전체' ? '난이도 전체' : selectedExamDifficultyContent}
+            <div
+              onClick={() => setIsExamDifficultyOpen(!isExamDifficultyOpen)}
+              className={'relative flex items-center rounded-full bg-white py-[6px] px-[12px]'}>
+              <div className={'text-h6'}>
+                {selectedExamDifficultyContent === '전체' ? '난이도 전체' : selectedExamDifficultyContent}
+              </div>
+              {isExamDifficultyOpen ? <ActivationIcon /> : <DisableIcon />}
+              {isExamDifficultyOpen ? (
+                <ExamDifficultyFilter
+                  selectedExamDifficulty={selectedExamDifficultyContent}
+                  setIsExamDifficultyOpen={setIsExamDifficultyOpen}
+                  setExamDifficulty={setExamDifficulty}
+                  setSelectedExamDifficultyContent={setSelectedExamDifficultyContent}
+                />
+              ) : null}
             </div>
-            {isExamDifficultyOpen ? <ActivationIcon /> : <DisableIcon />}
-            {isExamDifficultyOpen ? (
-              <ExamDifficultyFilter
-                selectedExamDifficulty={selectedExamDifficultyContent}
-                setIsExamDifficultyOpen={setIsExamDifficultyOpen}
-                setExamDifficulty={setExamDifficulty}
-                setSelectedExamDifficultyContent={setSelectedExamDifficultyContent}
-              />
-            ) : null}
           </div>
-        </div>
+        )}
         {examReviewsCRT_003Error ? (
-          <div className={'flex flex-col justify-center items-center min-h-[500px] text-h3 text-gray4'}>
-            관리자가 시험 정보를 입력하지 않았어요.
+          <div className={'flex flex-col justify-center items-center min-h-[600px] text-h3 text-gray4'}>
+            시험 정보가 곧 등록될 예정이에요.
           </div>
         ) : examReviewsCRT_004Error ? (
-          <div className={'flex flex-col justify-center items-center min-h-[500px] text-h3 text-gray4'}>
-            따끈 후기 작성 기간이 아니에요.
+          <div className={'flex flex-col justify-center items-center min-h-[600px] text-h3 text-gray4'}>
+            따끈후기는 시험 이후에 작성할 수 있어요.
           </div>
         ) : (
           <div className={'flex flex-col gap-y-4'}>
