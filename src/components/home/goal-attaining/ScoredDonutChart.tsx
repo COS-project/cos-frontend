7e549e8dd7cdb68a738056878410502bc38be8b4
유림 <yulimh001@gmail.com>
2025-04-17 12:28:42 +0900

    fix: 목표 달성도 0일때 그래프 보이지 않는 문제 해결

--- src/components/home/goal-attaining/ScoredDonutChart.tsx
+++ src/components/home/goal-attaining/ScoredDonutChart.tsx
@@ -1,18 +1,19 @@
 import DonutChart from './donutchart';
 
 interface DonutChartProps {
-  mainscore: number | undefined;
-  totalscore: number;
+  maxScore: number | undefined;
+  goalScore: number;
 
   unit: string;
 }
 
-const ScoredDonutChart: React.FC<DonutChartProps> = ({ mainscore, totalscore, unit }) => {
-  const chartData = []; // 예시 데이터
-  if (mainscore) {
-    chartData[0] = totalscore - mainscore;
-    chartData[1] = mainscore;
-  }
+const ScoredDonutChart: React.FC<DonutChartProps> = ({ maxScore, goalScore, unit }) => {
+  const chartData =
+    typeof maxScore === 'number'
+      ? goalScore - maxScore <= 0
+        ? [0, 1] // 파랑 100%
+        : [goalScore - maxScore, maxScore] // 회색 + 파랑
+      : [1, 0]; // undefined일 경우 대비 (회색 100%)
 
   return (
     <div>
@@ -20,11 +21,11 @@ const ScoredDonutChart: React.FC<DonutChartProps> = ({ mainscore, totalscore, un
       {/* 가운데에 위치할 텍스트 */}
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
         <p className="font-semibold text-h3">
-          {mainscore}
+          {maxScore}
           {unit}
         </p>
         <p className="text-h6 text-gray4">
-          /{totalscore}
+          /{goalScore}
           {unit}
         </p>
       </div>
