0df46cabdf638a00afe57b38638bc2603b6fd07e
유림 <yulimh001@gmail.com>
2025-04-24 16:42:35 +0900

    fix: 내가 쓴 글 api 파라미터명 변경

--- src/lib/hooks/useGetUserPosts.tsx
+++ src/lib/hooks/useGetUserPosts.tsx
@@ -12,7 +12,7 @@ const getKey = (
 ) => {
   // 초기 요청
   if (pageIndex === 0) {
-    return `/api/v2/${postType}/posts/my-posts?page=${pageIndex}&size=10&sortKey=createdAt, id&sortDirections=${sortDirections}`;
+    return `/api/v2/${postType}/posts/my-posts?page=${pageIndex}&size=10&sortFields=createdAt, id&sortDirections=${sortDirections}`;
   }
 
   // 이전 페이지 데이터가 없으면 종료
@@ -20,7 +20,7 @@ const getKey = (
 
   // 이전 페이지에 더 많은 데이터가 있으면 다음 페이지 요청
   if (previousPageData.result.hasNext) {
-    return `/api/v2/${postType}/posts/my-posts?page=${pageIndex}&size=10&sortKey=createdAt, id&sortDirections=${sortDirections}`;
+    return `/api/v2/${postType}/posts/my-posts?page=${pageIndex}&size=10&sortFields=createdAt, id&sortDirections=${sortDirections}`;
   }
 
   // 이전 페이지에 더 이상 데이터가 없으면 null 반환
